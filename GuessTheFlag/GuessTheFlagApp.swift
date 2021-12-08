812b0a4ff514d78fdb72fc79fd6d685cd6cca96d
Daniel JCH <danielj.callisayah@gmail.com>
2021-12-08 09:33:02 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
