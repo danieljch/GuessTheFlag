c5318d19f25a77c18d4a70ffce3d0e87213f51a6
Daniel JCH <danielj.callisayah@gmail.com>
2021-12-10 12:18:22 -0400

    Title shadow added

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -20,8 +20,8 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+                .init(color: Color(red: 0.41, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.16), location: 0.3),
             ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
             VStack {
@@ -114,6 +114,7 @@ struct LargeTitle: ViewModifier{
         content
             .font(.largeTitle)
             .foregroundColor(.blue)
+            .shadow(color: .white, radius: 3, x: -2, y: 0)
     }
 }
 
